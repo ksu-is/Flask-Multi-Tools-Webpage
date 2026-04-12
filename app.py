830806ff7c70cdb66f9c823320bab8e84676fba7
Quang Hai Huynh <qhuynh6@students.kennesaw.edu>
2026-04-12 04:53:54 -0400

    transfer codes from seperate repository

--- app.py
+++ app.py
@@ -1,116 +1,111 @@
-from flask import Flask, render_template, redirect, url_for, request, Response, send_from_directory
+from flask import Flask, render_template, request, redirect
+from miscellaneous.states import us_states
+import random
 import pandas as pd
-import os
-import uuid
-from flask import session, make_response, flash
 
-app = Flask(__name__, template_folder='templates', static_folder='static', static_url_path='/')
-app.secret_key = "luno"
+app = Flask(__name__, static_folder='static', static_url_path= '/', template_folder='templates')
 
-@app.route('/', methods = ['GET','POST'])
+@app.route('/', methods=['GET','POST'])
 def home():
-    if request.method == 'GET':
-        names = ["Chico",'Luno','Augusta','Charlotte Hazelrink']
-        return render_template('home.html', list = names, message = 'Index')
-    elif request.method == 'POST':
-        id = request.form.get('username')
-        pas = request.form.get('password')
+    yourname = ''
+    if request.method == 'POST':
+        yourname = request.form.get('yourname')
+    return render_template('home.html', message= yourname)
 
-        if id == 'luno' and pas == '1234':
-            return 'correct'
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/state_abbreviation', methods=['GET','POST'])
+def state_abbreviation():
+    full_state_name = ''
+    abbreviation = ''
+    if request.method == 'POST':
+        abbreviation = request.form.get('state_abbreviation')
+        full_state_name = us_states[abbreviation]
+    return render_template('state_abbreviation.html', us_states=us_states, full_state_name=full_state_name, abbreviation=abbreviation)
+
+@app.route('/calculator',methods=['GET','POST'])
+def calculator():
+    result =''
+    first_number = ''
+    second_number = ''  
+    operator = ''
+    if request.method == 'POST':
+        first_number = request.form.get("first_number")
+        second_number = request.form.get("second_number")
+        operator = request.form.get("operator")
+
+        if operator == "+":
+            result = float(first_number) + float(second_number)
+        elif operator == "-":
+            result = float(first_number) - float(second_number)
+        elif operator == "X":
+            result = float(first_number) * float(second_number)
         else:
-            return 'incorrect'
-        
+            if float(second_number) == 0:
+                result = "Error - Cannot divide by zero"
+            else:
+                result = float(first_number) / float(second_number)
+       
+    return render_template('calculator.html',result=result,first_number=first_number,second_number=second_number,operator=operator)
+
+@app.route('/guess_number', methods=['GET','POST'])
+def guess_number():
+    level = ''
+    attempts = '' 
+    target = '' 
+    result = '' 
+    guess = '' 
+    count = ''
+    if request.method == 'POST':
+        level = int(request.form.get('level'))
+        attempts = int(request.form.get('attempts'))
+        guess = int(request.form.get('guess', 0))
+        count = int(request.form.get('count', 0))
+        target = int(request.form.get('target',0))
+
+        if not target:
+            if level == 1:
+                target = random.randint(1, 9)
+            elif level == 2:
+                target = random.randint(10, 99)
+            else:
+                target = random.randint(100, 999)
+
+        if guess:
+            if count < attempts:
+                if guess == target:
+                    result = "Congratulations! You guessed the number."
+                else:                  
+                    result = "Sorry, that's not the correct number. Try again."
+                    count += 1
+            if count >= attempts:
+                result = f"Out of attempts - the number was {target}."
+
+    return render_template('guess_number.html', level=level, result=result, attempts=attempts, guess=guess, count=count, target=target)
+
+@app.route('/excel_upload', methods=['GET', 'POST'])
+def excel_upload():
+    if request.method == 'POST':
+        file = request.files['file']
+        df = pd.read_excel(file)
+        return df.to_html()
+    return render_template('excel_upload.html')
+
 @app.route('/image')
 def image():
     return render_template('image.html')
 
-
-
-@app.route('/loli')
-def LOLI():
-    return render_template('charlotte.html')
-
-@app.route('/redirect')
-def redirect1():
-    return redirect(url_for('LOLI'))
-
-@app.route('/upload', methods = ['POST'])
-def upload():
-    file = request.files['file']
-    df = pd.read_excel(file)
-    return df.to_html()
-
-@app.route('/csv_converter', methods = ['POST'])
-def csv_converter():
-    file = request.files['file']
-    df = pd.read_excel(file)
-    if not os.path.exists('downloads'):
-        os.makedirs('downloads')
-    filename = f"{uuid.uuid4()}.csv"
-    filepath = os.path.join('downloads', filename)
-    df.to_csv(filepath)
-    return render_template('download.html', filename=filename)
-
-@app.route('/set_data')
-def set_data():
-    session['name'] = 'augusta'
-    session['hair'] = 'red'
-    return render_template('home.html', message = 'Session data set')
-
-@app.route('/get_data')
-def get_data():
-    if 'name' in session.keys() and 'hair' in session.keys():
-        name = session['name']
-        hair = session['hair']
-        return render_template('home.html', message = f"Name: {name}, Hair: {hair}")
-    else:
-        return render_template('home.html', message = 'No session data found')
-
-@app.route('/clear_session')
-def clear_session():
-    session.clear()
-    return render_template('home.html', message = 'Session cleared')
-
-@app.route('/set_cookie')
-def set_cookie():
-    response = make_response(render_template('home.html', message = 'Cookie set'))
-    response.set_cookie(key='cookie key', value='cookie value')
-    return response
-
-@app.route('/get_cookie')
-def get_cookie():
-    cookie_value = request.cookies['cookie key']
-    return render_template('home.html', message = f"Cookie value: {cookie_value}")
-
-@app.route('/clear_cookie')
-def clear_cookie():
-    response = make_response(render_template('home.html', message = 'Cookie cleared'))
-    response.set_cookie(key='cookie key', expires=0)
-    return response
-
-@app.route('/login', methods=['GET', 'POST'])
-def log_in():
-    if request.method == 'GET':
-        return render_template('login.html')
-    elif request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
-        if username == 'luno' and password == '1234':
-            flash('Logged in successfully')
-            return render_template('image.html')
-        else:
-            flash('Log in failed')
-            return render_template('image.html')
-
-
-
-
-
-    
-    
-
-
-
 if __name__ == '__main__':
-    app.run(debug=True) 
+    app.run(debug=True)
+
+
+
+
+
